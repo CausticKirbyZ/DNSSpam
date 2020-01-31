5b3b0f16e454461a5fdcc63c571c0f94dbbb685d
gido5731 <gido5731@gido-desk-deb-v2.imagen.click>
2020-01-30 23:54:26 -0500

    Fixed Broken option and converted file to unix

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-wget https://www.domcop.com/files/top/top10milliondomains.csv.zip --no-check-certificate
-unzip top10milliondomains.csv.zip 
+wget https://www.domcop.com/files/top/top10milliondomains.csv.zip
+unzip top10milliondomains.csv.zip
